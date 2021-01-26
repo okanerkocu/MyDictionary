2daa51c10e02a63373879db02b89e8cd275be9aa
Okan Erkocu <okanerkocu@hotmail.com>
2021-01-26 23:30:56 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T, L>
+    {
+        T[] keys;
+        L[] values;
+
+        public MyDictionary()
+        {
+            keys = new T[0];
+            values = new L[0];
+        }
+
+        public void Add(T key, L value)
+        {
+            T[] tempKey = keys;
+            keys = new T[tempKey.Length + 1];
+            L[] tempValue = values;
+            values = new L[tempValue.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+                values[i] = tempValue[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("İsim : " + keys[i]+", Numara : "+values[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+            myDictionary.Add("Mehmet", 1);
+            myDictionary.Add("Ayşe", 2);
+            myDictionary.Add("Ali", 3);
+            myDictionary.List();
+        }
+    }
+}
